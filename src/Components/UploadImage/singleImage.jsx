d701514b70dcf9825da98f08d6d67a2a877a8d12
Chirag <takchirag828@gmail.com>
2023-02-24 20:48:42 +0530

    complete market admit main page

--- src/Components/UploadImage/singleImage.jsx
+++ src/Components/UploadImage/singleImage.jsx
@@ -3,12 +3,12 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import './Image.css'
 import { NameSeperator } from '../../helpers/filesNameSeperator';
 
-const SingleImageSelector = ({setImageVlaue}) => {
+const SingleImageSelector = ({getImage}) => {
     const [name,setName]=useState('')
     const handleImageValue = (e) => {
         const namesArray=NameSeperator(e.target.files[0].type);
         if(namesArray[0] === "image"){
-            setImageVlaue(e.target.files[0]);
+            getImage(e.target.files[0]);
             setName(e.target.files[0].name)
         }
         else{
@@ -20,8 +20,8 @@ const SingleImageSelector = ({setImageVlaue}) => {
    <>
     <div className="image_upload">
         <div className="image">
-            <input  type="file" id='file' onChange={handleImageValue}/>
-            <label htmlFor="file">{name?name:"Select Single Image"} <AddCircleIcon style={{color:"blue", fontSize:"35px", cursor:"pointer"}}/></label>
+            <input  type="file" id='file_1' onChange={handleImageValue}/>
+            <label htmlFor="file_1">{name?name:"Select Single Image"} <AddCircleIcon style={{color:"blue", fontSize:"35px", cursor:"pointer"}}/></label>
         </div> 
     </div>
    </>
