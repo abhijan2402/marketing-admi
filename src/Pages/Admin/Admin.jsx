9fbfc9aec575e241cc15e291791ef49f3d3e3774
Chirag <takchirag828@gmail.com>
2023-02-25 21:47:30 +0530

    file adding complete

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -8,131 +8,85 @@ import { collection, addDoc } from "firebase/firestore";
 import { db,storage } from '../../firebase';
 import { ref,getDownloadURL,uploadBytesResumable } from "firebase/storage";
 import SingleImageSelector from '../../Components/UploadImage/singleImage';
+import { async } from '@firebase/util';
 
 
 const Admin = () => {
     const [images,setImages]=useState([]);
     const [videos,setVideos]=useState([]);
     const [heading,setHeading]=useState('');
+    const [projectName,setProjectName]=useState('');
     const [subHeading,setSubHeading]=useState('');
     const [titleImage,setTitleImage]=useState('');
 
-    const [imaegsDownLoadUrls,setImaegsDownLoadUrls]=useState([]);
-    const [videoDownLoadUrls,setVidoeDownLoadUrls]=useState([]);
-
     const [loading,setLoading]=useState(false)
-    async function createOrderItems(images,videos,titleImageUrl) {
-        try {
-            await addDoc(collection(db, "orderItems"), {
-                heading:heading,
-                subHeading:subHeading,
-                images:images,
-                videos:videos,
-                titleMainImage:titleImageUrl
-              })
-              .then((docRef) => {
-                setLoading(false)
-                alert("Post Added")
-            })
-              
-        } catch (error) {
-            setLoading(false)
-            console.log(error);
-        }
+    const createData=async(images,videos,titleImg)=>{
+        return addDoc(collection(db, "orderItems"), {
+            projectName:projectName,
+            heading:heading,
+            subHeading:subHeading,
+            images:images,
+            videos:videos,
+            titleMainImage:titleImg
+        })
+        .then((docRef) => {
+            return docRef.id
+        })
+        .catch((e)=>{
+            console.log(e)
+        })
     }
-    const setCompleteData=async(images,videos)=>{
-        try {
-            const storageRef = ref(storage, `/file/${titleImage.name}`);
-            const uploadTask = uploadBytesResumable(storageRef, titleImage);
-            return uploadTask.on('state_changed', 
+    const getDownloadUrl=async(file)=>{
+        const storageRef = ref(storage, `/file/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        return new Promise((resolve,reject)=>{uploadTask.on('state_changed', 
                 (snapshot)=>{},
                 (error) => {
-                    console.log(error)
+                    reject(error)
                 }, 
                 () => {
                     return getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        createOrderItems(images,videos,downloadURL)
+                        resolve(downloadURL)
+                    })
+                    .catch((e)=>{
+                        reject(e)
                     })
                 }
             );
-        } catch (error) {
-            setLoading(false)
-            console.log(error);
-        }
+        })
     }
-    const getDownloadUrlOfImage=async()=>{
+
+    async function processFiles(filesArray){
+        console.log("Star")
+        let fileUrlArray=[];
+        for(let i = 0; i < filesArray.length; i++){
+            let result = await getDownloadUrl(filesArray[i]);
+            fileUrlArray.push(result);
+        }
+        return fileUrlArray
+    }
+    
+    async function uploadData(){
         setLoading(true)
-        try {
-            if(heading==='' || subHeading === '' || titleImage==="")
-                throw "Heading , SubHeading and title image is mandaory"
-            if(images.length){
-                console.log("image")
-                let imagesLink=[];
-                images.forEach((image)=>{
-                    const storageRef = ref(storage, `/file/${image.name}`);
-                    const uploadTask = uploadBytesResumable(storageRef, image);
-                    return uploadTask.on('state_changed', 
-                        (snapshot)=>{},
-                        (error) => {
-                            console.log(error)
-                        }, 
-                        () => {
-                            return getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                                imagesLink.push(downloadURL)
-                            }).then(()=>{
-                                setImaegsDownLoadUrls(imagesLink)
-                                if(videos.length){
-                                    getDownloadUrlOfVideo(imagesLink)
-                                }
-                                else{
-                                    setCompleteData(imagesLink,[])
-                                }
-                            })
-                        }
-                    );
-                })
-            }
-            else{
-                if(videos.length){
-                    getDownloadUrlOfVideo([])
-                }
-                else{
-                    setCompleteData([],[])
-                }
-            }
-        } catch (error) {
-            setLoading(false)
-            alert(error)
+       try{
+        if(heading=='' || subHeading==='' ){
+            alert("Please give title , subTitle and project Image");
+            return;
         }
+        const videosUrlArray=await processFiles(videos);
+        const imagesUrlArray=await processFiles(images);
+        const titleImageUrl=await getDownloadUrl(titleImage);
+        let res=await createData(imagesUrlArray,videosUrlArray,titleImageUrl);
+        if(res)
+            setLoading(false);
+       }catch(e){
+        setLoading(false)
+        alert("Something gonna wrong")
+        console.log(e)
+       }
+
     }
-    const getDownloadUrlOfVideo=(images)=>{
-        try {
-            if(videos.length){
-                videos.forEach((video)=>{
-                    let videoLink=[];
-                    const storageRef = ref(storage, `/file/${video.name}`);
-                    const uploadTask = uploadBytesResumable(storageRef, video);
-                    uploadTask.on('state_changed', 
-                        (snapshot)=>{},
-                        (error) => {
-                            console.log(error)
-                        }, 
-                        () => {
-                            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                                videoLink.push(downloadURL)
-                            }).then(()=>{
-                                setVidoeDownLoadUrls(videoLink)
-                                setCompleteData(images,videoLink)
-                            })
-                        }
-                    );
-                })
-            }
-        } catch (error) {
-            setLoading(false)
-            console.log(error)
-        }
-    }
+    
     return (
         <>
             <div className="main_container">
@@ -149,10 +103,12 @@ const Admin = () => {
                     </div>
                     <div className="admin_details">
                         <form>
+                            <input type="text" placeholder='Project Name' onChange={(projectName)=>setProjectName(projectName.target.value)} /> <br />
                             <input type="text" placeholder='Heading' onChange={(heading)=>setHeading(heading.target.value)} /> <br />
                             <input type="text" placeholder='Sub Heading' onChange={(subheading)=>setSubHeading(subheading.target.value)}/>
                             <SingleImageSelector
                                 getImage={(singleImage)=>{
+                                    console.log(singleImage)
                                     setTitleImage(singleImage)
                                 }}
                             />
@@ -175,9 +131,11 @@ const Admin = () => {
                         </form>
                         <Button variant="contained"
                             disabled={loading}
-                            onClick={getDownloadUrlOfImage}
+                            onClick={uploadData}
                         >
-                            Add Project
+                            {
+                                loading?"Uploading Files do not refresh":"Add Project"
+                            }
                         </Button>
                     </div>
                 </div>
