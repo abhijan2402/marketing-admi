c1e94a62a87a0ed8b90d02138659456ce3d4be4f
abhijan2402 <abhishek.jangid643@gmail.com>
2023-03-04 00:24:09 +0530

    adding Image deletion feature

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -3,9 +3,11 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import ReactPlayer from "react-player";
 import Pic2 from "../../assest/download.jfif";
 import video from "../../assest/video1.mp4";
-import { db, storage } from "../../firebase";
 import "./project.css";
 import { collection, query, getDocs } from "firebase/firestore";
+import { ref, deleteObject } from "firebase/storage";
+import { db, storage } from "../../firebase";
+import { doc, deleteDoc } from "firebase/firestore";
 const Project = () => {
   useEffect(() => {
     getData();
@@ -19,6 +21,57 @@ const Project = () => {
       resultArray.push({ id: doc.id, ...doc.data() });
     });
     setdata(resultArray);
+    console.log(data)
+  };
+  const delData = async (item) => {
+    console.log(item);
+    try {
+      await StorageDeletion(item);
+      await deleteDoc(doc(db, "orderItems", item.id));
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const StorageDeletion = async (item) => {
+    try {
+      const ImageData = item.titleMainImage;
+      const ID = JSON.stringify(ImageData);
+      const myArray = ID.split("file%2F", 2);
+      let word = myArray[1];
+      const check = word.split("?");
+      // console.log(check[0]);
+      const desertRef = ref(storage, `file/${check[0]}`);
+      await deleteObject(desertRef)
+        .then(() => {
+          console.log("deleted Title Images");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      const arrayImage = item.images
+      console.log(arrayImage, "i am arry image");
+      let k = 0;
+      for (k = 0; k < arrayImage.length; k++) {
+        const ImageDataArr = arrayImage[k];
+        const IDArr = JSON.stringify(ImageDataArr);
+        const myArrayArr = IDArr.split("file%2F", 2);
+        let wordArr = myArrayArr[1];
+        const checkArr = wordArr.split("?");
+        // console.log(checkArr[0], 'I am in gthe array');
+        const desertRef = ref(storage, `file/${checkArr[0]}`);
+        await deleteObject(desertRef)
+          .then(() => {
+            console.log("deleted Array Images");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <>
@@ -31,7 +84,9 @@ const Project = () => {
               <div key={item.id} className="project_list">
                 <div className="project_name">
                   <label htmlFor="text">{item.projectName}</label>
-                  <DeleteIcon style={{ color: "red" }} />
+                  <DeleteIcon style={{ color: "red" }} onClick={() => {
+                    delData(item);
+                  }} />
                 </div>
                 <div className="heading">
                   <p style={{ fontWeight: "700", width: "100px" }}>Heading:</p>
@@ -61,8 +116,8 @@ const Project = () => {
               <div className="vid_player">
                 {
                   item.videos.length &&
-                  item.videos.map((vid)=>(
-                    <div style={{width:"90%",height:'70vh',resize: 'both',display:"flex",alignItems:"center",justifyContent:"center",display:"column"}}> 
+                  item.videos.map((vid) => (
+                    <div style={{ width: "90%", height: '70vh', resize: 'both', display: "flex", alignItems: "center", justifyContent: "center", display: "column" }}>
                       <video controls autoPlay={true} >
                         <source src={vid} />
                       </video>
