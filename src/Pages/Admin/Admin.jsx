2c6c845fdf128b28d81cabbc0dfd77dcf92cbc18
abhijan2402 <abhishek.jangid643@gmail.com>
2023-03-04 00:38:37 +0530

    adingsome chnahed

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,112 +1,112 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Button from '@mui/material/Button';
 import Image from '../../Components/UploadImage/Image';
 import Video from '../../Components/UploadVideo/Video';
 import FilesNameContainer from '../../Components/Project/FilesNamesContainer';
-import { collection, addDoc } from "firebase/firestore"; 
-import { db,storage } from '../../firebase';
-import { ref,getDownloadURL,uploadBytesResumable } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
+import { db, storage } from '../../firebase';
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import SingleImageSelector from '../../Components/UploadImage/singleImage';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const months=["January","February","March","April","May","June","July","August","September","October","November","Decemnber"]
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decemnber"]
 const Admin = () => {
-    const date=new Date();
-    const [images,setImages]=useState([]);
-    const [videos,setVideos]=useState([]);
-    const [heading,setHeading]=useState('');
-    const [projectName,setProjectName]=useState('');
-    const [subHeading,setSubHeading]=useState('');
-    const [titleImage,setTitleImage]=useState('');
+    const date = new Date();
+    const [images, setImages] = useState([]);
+    const [videos, setVideos] = useState([]);
+    const [heading, setHeading] = useState('');
+    const [projectName, setProjectName] = useState('');
+    const [subHeading, setSubHeading] = useState('');
+    const [titleImage, setTitleImage] = useState('');
 
-    const [loading,setLoading]=useState(false)
-    const createData=async(images,videos,titleImg)=>{
+    const [loading, setLoading] = useState(false)
+    const createData = async (images, videos, titleImg) => {
         return addDoc(collection(db, "orderItems"), {
-            projectName:projectName,
-            heading:heading,
-            subHeading:subHeading,
-            images:images,
-            videos:videos,
-            titleMainImage:titleImg,
-            uploadedData:`${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+            projectName: projectName,
+            heading: heading,
+            subHeading: subHeading,
+            images: images,
+            videos: videos,
+            titleMainImage: titleImg,
+            uploadedData: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
         })
-        .then((docRef) => {
-            return docRef.id
-        })
-        .catch((e)=>{
-            toast("Some thing gonna wrond Please try again")
-        })
+            .then((docRef) => {
+                return docRef.id
+            })
+            .catch((e) => {
+                toast("Some thing gonna wrond Please try again")
+            })
     }
-    const getDownloadUrl=async(file)=>{
+    const getDownloadUrl = async (file) => {
         const storageRef = ref(storage, `/file/${file.name}`);
         const uploadTask = uploadBytesResumable(storageRef, file);
-        return new Promise((resolve,reject)=>{
-            uploadTask.on('state_changed', 
-                (snapshot)=>{},
+        return new Promise((resolve, reject) => {
+            uploadTask.on('state_changed',
+                (snapshot) => { },
                 (error) => {
                     reject(error)
-                }, 
+                },
                 () => {
                     return getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                         resolve(downloadURL)
                     })
-                    .catch((e)=>{
-                        reject(false)
-                    })
+                        .catch((e) => {
+                            reject(false)
+                        })
                 }
             );
         })
     }
 
-    async function processFiles(filesArray){
+    async function processFiles(filesArray) {
         try {
-            let fileUrlArray=[];
-            for(let i = 0; i < filesArray.length; i++){
+            let fileUrlArray = [];
+            for (let i = 0; i < filesArray.length; i++) {
                 let result = await getDownloadUrl(filesArray[i]);
-                if(result)
+                if (result)
                     fileUrlArray.push(result);
                 else
                     throw "Some thing gonna wrond Please try again"
             }
-            return fileUrlArray   
+            return fileUrlArray
         } catch (error) {
             toast(error)
         }
     }
-    
-    async function uploadData(){
+
+    async function uploadData() {
         setLoading(true)
-       try{
-        if( projectName==='' ||heading=='' || subHeading==='' || titleImage===''){ 
-            setLoading(false)
-            toast("Please Give Project name , title , subtitle and One Heading Image");
-            return;
-        }
-        toast("Uploading start")
-        const videosUrlArray=await processFiles(videos);
-        const imagesUrlArray=await processFiles(images);
-        const titleImageUrl=await getDownloadUrl(titleImage);
-        let res=await createData(imagesUrlArray,videosUrlArray,titleImageUrl);
-        if(res){
-            toast("Data Added")
-            resetField()
-            setLoading(false);
-        }
-        else{
-            throw "Some thing gonna wrond Please try again"
-        }
-       }catch(e){
+        try {
+            if (projectName === '' || heading == '' || subHeading === '' || titleImage === '') {
+                setLoading(false)
+                toast("Please Give Project name , title , subtitle and One Heading Image");
+                return;
+            }
+            toast("Uploading start")
+            const videosUrlArray = await processFiles(videos);
+            const imagesUrlArray = await processFiles(images);
+            const titleImageUrl = await getDownloadUrl(titleImage);
+            let res = await createData(imagesUrlArray, videosUrlArray, titleImageUrl);
+            if (res) {
+                toast("Data Added")
+                resetField()
+                setLoading(false);
+            }
+            else {
+                throw "Some thing gonna wrond Please try again"
+            }
+        } catch (e) {
             setLoading(false)
             toast(e)
             console.log(e)
-       }
+        }
 
     }
-    
-    const resetField=()=>{
+
+    const resetField = () => {
         setProjectName('');
         setHeading('');
         setSubHeading('');
@@ -129,42 +129,42 @@ const Admin = () => {
                 <div className="admin_page">
                     <h5>Admin page</h5>
                     <div className="admin_access_button">
-                    <p>Add Your Projects</p>
+                        <p>Add Your Projects</p>
                         <div className="access_buttons">
-                            <Link to='/Feedback' style={{textDecoration:"none"}}><Button variant="contained">Feedback</Button></Link>
-                            <Link to='/ProjectList' style={{textDecoration:"none"}}><Button variant="contained">See List</Button></Link>
+                            <Link to='/marketing-admi/Feedback' style={{ textDecoration: "none" }}><Button variant="contained">Feedback</Button></Link>
+                            <Link to='/marketing-admi/Projectlist' style={{ textDecoration: "none" }}><Button variant="contained">See List</Button></Link>
                         </div>
 
                     </div>
                     <div className="admin_details">
                         <form>
-                            <input value={projectName} type="text" placeholder='Project Name' onChange={(projectName)=>setProjectName(projectName.target.value)} /> <br />
-                            <input value={heading} type="text" placeholder='Heading' onChange={(heading)=>setHeading(heading.target.value)} /> <br />
+                            <input value={projectName} type="text" placeholder='Project Name' onChange={(projectName) => setProjectName(projectName.target.value)} /> <br />
+                            <input value={heading} type="text" placeholder='Heading' onChange={(heading) => setHeading(heading.target.value)} /> <br />
                             <textarea style={{
-                                width:"95%",
-                                borderColor:"black",
-                                fontWeight:"bold",
-                                borderRadius:5,
-                                marginTop:10,
-                                padding:10
-                            }} value={subHeading} type="text" placeholder='Sub Heading' onChange={(subheading)=>setSubHeading(subheading.target.value)} />
+                                width: "95%",
+                                borderColor: "black",
+                                fontWeight: "bold",
+                                borderRadius: 5,
+                                marginTop: 10,
+                                padding: 10
+                            }} value={subHeading} type="text" placeholder='Sub Heading' onChange={(subheading) => setSubHeading(subheading.target.value)} />
                             <SingleImageSelector
                                 titleImage={titleImage}
-                                getImage={(singleImage)=>{
+                                getImage={(singleImage) => {
                                     setTitleImage(singleImage)
                                 }}
                             />
                             <Image
-                                setImageArray={(image)=>{
-                                    setImages([...images,image])
+                                setImageArray={(image) => {
+                                    setImages([...images, image])
                                 }}
                             />
-                             <FilesNameContainer
+                            <FilesNameContainer
                                 fileNameArray={images}
                             />
                             <Video
-                                setVideosArray={(video)=>{
-                                    setVideos([...videos,video])
+                                setVideosArray={(video) => {
+                                    setVideos([...videos, video])
                                 }}
                             />
                             <FilesNameContainer
@@ -172,9 +172,9 @@ const Admin = () => {
                             />
                         </form>
                         {
-                           loading?
-                           <h1 style={{fontSize:15,textAlign:"center",cursor:"pointer"}}>Please wait you post is creating, Don't refresh the page</h1>:
-                           <Button variant="contained" disabled={loading} onClick={uploadData} >  Add Project </Button>
+                            loading ?
+                                <h1 style={{ fontSize: 15, textAlign: "center", cursor: "pointer" }}>Please wait you post is creating, Don't refresh the page</h1> :
+                                <Button variant="contained" disabled={loading} onClick={uploadData} >  Add Project </Button>
                         }
                     </div>
                 </div>
--- src/Pages/Admin/AdminAccess.jsx
+++ src/Pages/Admin/AdminAccess.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Button from '@mui/material/Button';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Admin.css';
 
 const AdminAccess = () => {
   return (
     <>
-        <div className="main_container">
-            <div className="admin_access">
-                <h1>Welcome to Marketing Admin Page</h1>
-                <Link to='/Admin' style={{textDecoration:"none"}}><Button variant="contained">Get Started</Button></Link>                
-            </div>
+      <div className="main_container">
+        <div className="admin_access">
+          <h1>Welcome to Marketing Admin Page</h1>
+          <Link to='/marketing-admi/Admin' style={{ textDecoration: "none" }}><Button variant="contained">Get Started</Button></Link>
         </div>
+      </div>
     </>
   )
 }
